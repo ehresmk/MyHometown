1a2b3eced8aee7f0474fb63ddee66b586d941655
Kevin <ehresmk1@tcnj.edu>
2020-03-22 19:47:41 -0400

    Attempted Login Functionality -.xml files added for;     -Main login screen     -Create account screen     -Home screen after login -Java files added for controlling the login process -Main login screen works fine     -Tells you when invalid username/password is entered -Create Account button crashes the app     -Could not fix at this time will fix at a later date

---
+++ MyHometown/app/src/main/java/com/app/MyHometown/App.java
@@ -0,0 +1,6 @@
+package com.app.MyHometown;
+
+import android.app;
+
+public class App extends app {
+}
---
+++ MyHometown/app/src/main/java/com/app/MyHometown/ui/login/LogoutActivity.java
@@ -0,0 +1,6 @@
+package com.app.MyHometown.ui.login;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class LogoutActivity extends AppCompatActivity {
+}
